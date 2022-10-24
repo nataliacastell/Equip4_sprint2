7ccda7e41042ac240a051e3d48fd19ae5b466e86
ncastell <nataliacastell@iesmontsia.org>
2022-10-24 17:48:20 +0200

    ok, falta fer test

--- files/PYTHON/kanban.py
+++ files/PYTHON/kanban.py
@@ -10,3 +10,19 @@ class Kanban:
         self.descripcioTasca = descripcioTasca
         self.estat = estat
         self.assignament = assignament
+    def selectKanban(id):
+        query = ("SELECT * FROM Kanban WHERE id.Kanban = %s", (id))
+        resultat = funciones.consultaDatos(query)
+        return resultat
+    def actualitzaRecomanacio(id, idTasca, nomTasca, descripcioTasca,estat, assignament):
+        query = ("UPDATE Kanban SET Id= %s, IdTasca= %s, nomTasca= %s, descripcioTasca= %s, estat= %s, assignment= %s "(id, idTasca, nomTasca, descripcioTasca,estat, assignament))
+        resultat = actualizarDatos(query)
+        return resultat
+    def inserirRecomanacio(id, idTasca, nomTasca, descripcioTasca,estat, assignament):
+        query = ("INSERT INTO Kanban VALUES (%s,%s,%s,%s,%s,%s) "(id, idTasca, nomTasca, descripcioTasca,estat, assignament))
+        resultat = actualizarDatos(query)
+        return resultat
+    def eliminaRecomanacio(id):
+        query = ("DELETE FROM Kanban WHERE id.Kanban = %s", (id))
+        resultat = actualizarDatos(query)
+        return resultat
