70b97a3bc6e8192c301e3ad595b17713f199506a
Francisco Zahinos <franciscozahinos@iesmontsia.org>
2022-11-02 19:15:38 +0100

    kanban progresos vagos

--- files/PHP/connexioBDD.php
+++ files/PHP/connexioBDD.php
@@ -1,9 +1,9 @@
 <?php
 // Connexio a base de dades
 $db_host="mariadb";
-$db="prymeralia";
-$user="security";
-$password="security";
+$db="oversegurity";
+$user="root";
+$password="root";
 
 $conn = mysqli_connect($db_host,$db,$user,$password);
 
