ae51873d8fc5943be1f56cb49407f6465c50a1a1
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2022-11-06 17:11:24 +0100

    menu_KivyMD Kanban

--- files/KivyMD/main.py
+++ files/KivyMD/main.py
@@ -3,14 +3,16 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.menu import MDDropdownMenu
 
 class ContentNavigationDrawer(MDBoxLayout):
     pass    
+
 
 class MyApp (MDApp):    
     def build(self):
         self.title = "PymeShield"
         return Builder.load_file("main2.kv")
-        
+
 
 MyApp().run()
