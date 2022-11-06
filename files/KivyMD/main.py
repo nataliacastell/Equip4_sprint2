a2e1176988da208033304807f001b561cf86cacc
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2022-11-06 23:26:57 +0100

    menu i pag principal kivymd

--- files/KivyMD/main.py
+++ files/KivyMD/main.py
@@ -1,13 +1,38 @@
 from turtle import color
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import Screen
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.menu import MDDropdownMenu
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import MDList
 
 class ContentNavigationDrawer(MDBoxLayout):
     pass    
 
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        '''Called when tap on a menu item.'''
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+        
+    def on_start(self):
+    icons_item = {
+        "folder": "My files",
+        "account-multiple": "Shared with me",
+        "star": "Starred",
+        "history": "Recent",
+        "checkbox-marked": "Shared with me",
+        "upload": "Upload",
+    }
+    for icon_name in icons_item.keys():
+        self.root.ids.content_drawer.ids.md_list.add_widgebatt(
+            ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+        )
+    
 
 class MyApp (MDApp):    
     def build(self):
