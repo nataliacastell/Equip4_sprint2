31cf86473a7c9639b72b9558db8fee88865e0102
ncastell <nataliacastell@iesmontsia.org>
2022-11-07 15:42:51 +0100

    connexiobdd modificat

--- files/PHP/connexioBDD.php
+++ files/PHP/connexioBDD.php
@@ -1,10 +1,13 @@
 <?php
 // Connexio a base de dades
-$db_host="mariadb";
-$db="oversegurity";
-$user="root";
-$password="root";
+  $db_host="mariadb"; //si no es "mariadb".
+  $db="oversegurity"; //nom de la base de dades
+  $user="root"; //usuari BDD
+  $pass="root"; //pass BDD
+  
+  $connexioDB = mysqli_connect($db_host, $user, $pass, $db);
 
-$conn = mysqli_connect($db_host,$user,$password,$db);
-
+  if ($connexioDB->connect_error) {
+    die("Connexió fallida: " . $connexioDB->connect_error);
+  }
 ?>
--- files/PHP/kanban.php
+++ files/PHP/kanban.php
@@ -7,52 +7,18 @@
         public $estat;
         public $assignament;
 
-        function setIDkanban($id){
-            $this->id = $id;
-        }
-        function getIDkanban(){
-            return $this->id;
-        }
-        function setIDtasca($idTasca){
-            $this->idTasca = $idTasca;
-        }
-        function getIDtasca(){
-            return $this->idTasca;
-        }
-        function setNomTasca($nomTasca){
-            $this->nomTasca = $nomTasca;
-        }
-        function getNomTasca(){
-            return $this->nomTasca;
-        }
-        function setDescripcioTasca($descripcioTasca){
-            $this->descripcioTasca = $descripcioTasca;
-        }
-        function getDescripcioTasca(){
-            return $this->descripcioTasca;
-        }
-        function setEstatTasca($estat){
-            $this->estat = $estat;
-        }
-        function getEstatTasca(){
-            return $this->estat;
-        }
-        function setAssignamentTasca($assignament){
-            $this->assignament = $assignament;
-        }
-        function getAssignamentTasca(){
-            return $this->assignament;
-        }
-        function newObjKanban($id, $idTasca, $nomTasca, $descripcioTasca, $estat, $assignament){
+       
+        public function __construct($id, $idTasca, $nomTasca, $descripcioTasca, $estat, $assignament){
             $arrayKanban[$contador]->id=$id;
             $arrayKanban[$contador]->idTasca=$idTasca;
             $arrayKanban[$contador]->nomTasca=$nomTasca;
             $arrayKanban[$contador]->descripcioTasca=$descripcioTasca;
             $arrayKanban[$contador]->estat=$estat;
             $arrayKanban[$contador]->assignament=$assignament;
-
-            $contarKanban ++;
             return $arrayKanban;
+        }
+        function consultarKanban($id){
+            
         }
     }
 ?>
