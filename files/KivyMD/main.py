78bd58add83e2df62aebf8fceaa26fe50355d6e3
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2022-11-07 18:00:16 +0100

    kivymd

--- files/KivyMD/main.py
+++ files/KivyMD/main.py
@@ -4,13 +4,14 @@ from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.list import MDList
+from kivymd.uix.list import OneLineIconListItem
+from kivy.uix.scrollview import ScrollView
 
 class ContentNavigationDrawer(MDBoxLayout):
     pass    
 
 class DrawerList(ThemableBehavior, MDList):
     def set_color_item(self, instance_item):
-        '''Called when tap on a menu item.'''
 
         # Set the color of the icon and text for the menu item.
         for item in self.children:
@@ -19,24 +20,22 @@ class DrawerList(ThemableBehavior, MDList):
                 break
         instance_item.text_color = self.theme_cls.primary_color
         
-    def on_start(self):
-    icons_item = {
-        "folder": "My files",
-        "account-multiple": "Shared with me",
-        "star": "Starred",
-        "history": "Recent",
-        "checkbox-marked": "Shared with me",
-        "upload": "Upload",
-    }
-    for icon_name in icons_item.keys():
-        self.root.ids.content_drawer.ids.md_list.add_widgebatt(
-            ItemDrawer(icon=icon_name, text=icons_item[icon_name])
-        )
-    
+
 
 class MyApp (MDApp):    
     def build(self):
         self.title = "PymeShield"
+
+        scroll = ScrollView()
+
+        list_view = MDList()
+        for i in range(20):
+
+            items = OneLineIconListItem(text=str(i) + ' item')
+            list_view.add_widget(items)
+
+        scroll.add_widget(list_view)
+
         return Builder.load_file("main2.kv")
 
 
