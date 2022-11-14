f711d12b3a62e2c7f15c1ffb45725e6ba1e97978
Francisco Zahinos <franciscozahinos@iesmontsia.org>
2022-11-14 19:25:31 +0100

    progresos gantt

--- files/HTML/DiagramaGantt.php
+++ files/HTML/DiagramaGantt.php
@@ -5,26 +5,26 @@ require_once("../PHP/TascaClass.php");
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pymeshield Gantt</title>
-    <?php require_once("head.php"); ?>
-    <script src="../JavaScript/gantt.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Pymeshield Gantt</title>
+  <?php require_once("head.php"); ?>
+  <script src="../JavaScript/gantt.js"></script>
 </head>
 
 <body class="d-flex flex-column min-vh-100">
-    <header class="sticky-top">
-        <?php require_once("header.php"); ?>
-    </header>
-    <main>
-        <p class="h1 text-center pb-3">{Nombre del informe}</p>
-        <div class="table-responsive rounded-top" id="gantt"></div>
-        <?php
-        $ganntt1 = new Tasca;
-        $modal = $ganntt1->listarGantt();
-        while ($row = mysqli_fetch_array($modal, MYSQLI_ASSOC)) {
-            echo '<div class="modal fade" id="modal' . $row["Id"] . '" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+  <header class="sticky-top">
+    <?php require_once("header.php"); ?>
+  </header>
+  <main>
+    <p class="h1 text-center pb-3">{Nombre del informe}</p>
+    <div class="table-responsive rounded-top" id="gantt"></div>
+    <?php
+    $ganntt1 = new Tasca;
+    $modal = $ganntt1->listarGantt();
+    while ($row = mysqli_fetch_array($modal, MYSQLI_ASSOC)) {
+      echo '<div class="modal fade" id="modal' . $row["Id"] . '" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
@@ -32,18 +32,25 @@ require_once("../PHP/TascaClass.php");
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body"><h2 class="fs-5"><i class="fa-regular fa-clipboard"></i>Descripción</h2><p>'
-                . $row["Descripcio"] .
-                '</p><hr><h2 class="fs-5"><i class="fa-regular fa-clock"></i>Fecha</h2><p>' . $row["DataInici"] . ' a ' . $row["DataFinal"] .
-                '</p></div>
+        . $row["Descripcio"] .
+        '</p><hr><h2 class="fs-5"><i class="fa-regular fa-clock"></i>Fecha</h2><p>' . $row["DataInici"] . ' a ' . $row["DataFinal"] .
+        '</p>
+                <hr><h2 class="fs-5"><label for="customRange2" class="form-label"><i class="fa-solid fa-percent"></i>Progreso</label></h2></p><p>
+                <form action="saveGantt.php" method="POST">
+                <input type="range" name="porcentaje" class="form-range" min="0" max="100" id="customRange'  . $row["Id"] .  '"></p></div>
+                <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="submit" class="btn btn-primary">Guardar cambios</button></form>
+      </div>
                 </div>
               </div>
             </div>';
-        }
-        ?>
-    </main>
-    <footer class="bg-black text-center text-lg-center mt-auto">
-        <?php require_once("footer.php"); ?>
-    </footer>
+    }
+    ?>
+  </main>
+  <footer class="bg-black text-center text-lg-center mt-auto">
+    <?php require_once("footer.php"); ?>
+  </footer>
 </body>
 
 </html>
