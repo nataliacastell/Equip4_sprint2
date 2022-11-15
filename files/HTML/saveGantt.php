a4e3881dcd6b7e7c237e3e070e36f0d81bd08618
Francisco Zahinos <franciscozahinos@iesmontsia.org>
2022-11-15 17:31:25 +0100

    xss

--- files/HTML/saveGantt.php
+++ files/HTML/saveGantt.php
@@ -2,7 +2,7 @@
 require_once("../PHP/TascaClass.php");
 if (isset($_GET['id'])) {
   $porcentaje = $_POST['porcentaje'];
-  $idGantt = $_GET['id'];
+  $idGantt = intval($_GET['id']);
   $tasca = new Tasca;
   $tasca->modificarPorcentaje($idGantt, $porcentaje);
   header("Location: DiagramaGantt.php");
